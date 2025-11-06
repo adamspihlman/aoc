b01708905ad6d7aec4369b54b63ef34366b39a79
Adam Spihlman <spihlman@umich.edu>
2025-11-05 20:23:26 -0600

    Add one obstacle test for day 6 part 2

--- src/bin/06.rs
+++ src/bin/06.rs
@@ -29,8 +29,16 @@ mod tests {
     }
 
     #[test]
-    fn test_part_two() {
+    fn test_part_two_default() {
         let result = part_two(&advent_of_code::template::read_file("examples", DAY));
         assert_eq!(result, Some(6));
+    }
+
+    #[test]
+    fn test_part_two_one_obstacle() {
+        let result = part_two(&advent_of_code::template::read_file_part(
+            "examples", DAY, 2,
+        ));
+        assert_eq!(result, Some(1));
     }
 }
