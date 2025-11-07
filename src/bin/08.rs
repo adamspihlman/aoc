467629c7cf623994bc732fda29a17f3527ed79bc
Adam Spihlman <spihlman@umich.edu>
2025-11-07 12:22:15 -0600

    Add day 8 part 2 test cases

--- src/bin/08.rs
+++ src/bin/08.rs
@@ -22,8 +22,26 @@ mod tests {
     }
 
     #[test]
+    #[ignore]
     fn test_part_two() {
         let result = part_two(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        assert_eq!(result, Some(34));
+    }
+
+    #[test]
+    #[ignore]
+    fn test_part_two_example_two() {
+        let result = part_two(&advent_of_code::template::read_file_part(
+            "examples", DAY, 2,
+        ));
+        assert_eq!(result, Some(9));
+    }
+
+    #[test]
+    fn test_part_two_slope_one() {
+        let result = part_two(&advent_of_code::template::read_file_part(
+            "examples", DAY, 3,
+        ));
+        assert_eq!(result, Some(9));
     }
 }
