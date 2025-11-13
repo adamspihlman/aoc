ebf14b4d9fdcbab5f5e21a7e3fbb588c2f0d29f9
Adam Spihlman <spihlman@umich.edu>
2025-11-12 22:39:38 -0600

    Add second example from spec for day 16 part 1

--- src/bin/16.rs
+++ src/bin/16.rs
@@ -1,10 +1,10 @@
 advent_of_code::solution!(16);
 
-pub fn part_one(input: &str) -> Option<u64> {
+pub fn part_one(_input: &str) -> Option<u64> {
     None
 }
 
-pub fn part_two(input: &str) -> Option<u64> {
+pub fn part_two(_input: &str) -> Option<u64> {
     None
 }
 
@@ -13,9 +13,19 @@ mod tests {
     use super::*;
 
     #[test]
+    #[ignore]
     fn test_part_one() {
         let result = part_one(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        assert_eq!(result, Some(7036));
+    }
+
+    #[test]
+    #[ignore]
+    fn test_part_one_example_two() {
+        let result = part_one(&advent_of_code::template::read_file_part(
+            "examples", DAY, 2,
+        ));
+        assert_eq!(result, Some(11048));
     }
 
     #[test]
