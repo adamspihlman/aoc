a67252756d89d81d2ce197285a63c1b379fcc2d6
Adam Spihlman <spihlman@umich.edu>
2025-11-15 11:44:36 -0600

    Fix day 18 part 1 test case setup

--- src/bin/18.rs
+++ src/bin/18.rs
@@ -60,7 +60,7 @@ mod tests {
 
     #[test]
     fn test_part_one_solution() {
-        let result = part_one(&advent_of_code::template::read_file("examples", DAY));
+        let result = part_one(&advent_of_code::template::read_file("inputs", DAY));
         assert_eq!(result, Some(372));
     }
 
