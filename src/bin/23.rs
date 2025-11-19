8898fcf908d3308e9f4c8900adb58b66a98a8a2f
Adam Spihlman <spihlman@umich.edu>
2025-11-18 20:33:29 -0600

    Add example test case for day 23 part 1

--- src/bin/23.rs
+++ src/bin/23.rs
@@ -1,10 +1,10 @@
 advent_of_code::solution!(23);
 
-pub fn part_one(input: &str) -> Option<u64> {
+pub fn part_one(_input: &str) -> Option<u64> {
     None
 }
 
-pub fn part_two(input: &str) -> Option<u64> {
+pub fn part_two(_input: &str) -> Option<u64> {
     None
 }
 
@@ -13,9 +13,10 @@ mod tests {
     use super::*;
 
     #[test]
+    #[ignore]
     fn test_part_one() {
         let result = part_one(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        assert_eq!(result, Some(23));
     }
 
     #[test]
